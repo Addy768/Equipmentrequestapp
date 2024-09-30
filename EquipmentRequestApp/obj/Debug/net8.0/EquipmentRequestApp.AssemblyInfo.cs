a4d6b9423ed31c3aef034f4c484398efa9c32898
Addy768 <adityasharma02092002@gmail.com>
2024-09-30 14:14:27 -0400

    all equipment page finished

--- EquipmentRequestApp/obj/Debug/net8.0/EquipmentRequestApp.AssemblyInfo.cs
+++ EquipmentRequestApp/obj/Debug/net8.0/EquipmentRequestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipmentRequestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54021a041608ff25e55966413cfac9ad7874fe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385d6d2c3185108a76427bbb553da49275e35736")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipmentRequestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipmentRequestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
